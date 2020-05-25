74671b3d73dfddbd8ce91205236f9dbab3513c35
Pankaj Pawar <P10484533@capita.co.uk>
2020-05-25 22:35:00 +0530

    Added model classes

---
+++ Models/ProfileModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ReferralSystem.Service;
+
+namespace ReferralSystem.Models
+{
+    public class Profile
+    {
+       public string CandidateName { get; set; }
+       public string UniqueID { get; set; }
+       public string BlobURI { get; set; }
+       public string AdditionalDetails { get; set; }
+       public string ReferredBy { get; set; }
+       public string DateReferred { get; set; }
+       public string JobID { get; set; }
+       public string Experience { get; set; }
+       public string Location { get; set; }
+       public string Screening1Status { get; set; }
+       public string Screening2Status { get; set; }
+       public string FinalRoundStatus { get; set; }
+       public string ProfileStatus { get; set; }
+       public string HRComment { get; set; }
+    }
+}
